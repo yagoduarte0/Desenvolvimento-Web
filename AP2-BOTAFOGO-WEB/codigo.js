6fce986396866f8e1dc44878ff7460c91770f899
Yago Duarte <yagoduarte1005@gmail.com>
2024-11-15 20:10:16 -0300

    commit inicio trabalho

--- AP2-BOTAFOGO-WEB/codigo.js
+++ AP2-BOTAFOGO-WEB/codigo.js
@@ -1,14 +1,20 @@
-const url = "https://botafogo-atletas.mange.li/2024-1/"
+const url = "https://botafogo-atletas.mange.li/2024-1/";
 
 const container = document.getElementById("container");
+const jogadoresSection = document.getElementById("jogadores");
+const loginForm = document.getElementById("login-form");
+const botaoLogout = document.getElementById("logout");
+const loginHeader = document.getElementById("header-login");
+const loginPage = document.getElementById("login-page");
+const login = document.getElementById("login");
 
 const manipulaClick = (e) => {
     const id = e.currentTarget.dataset.id;
     const url = `detalhes.html?id=${id}`;
 
-    document.cookie = `id=${id}`
-    document.cookie = `altura=${e.currentTarget.dataset.altura}`
-    
+    document.cookie = `id=${id}`;
+    document.cookie = `altura=${e.currentTarget.dataset.altura}`;
+
     localStorage.setItem(`id`, id);
     localStorage.setItem(`dados`, JSON.stringify(e.currentTarget.dataset));
 
@@ -16,26 +22,22 @@ const manipulaClick = (e) => {
     sessionStorage.setItem(`dados`, JSON.stringify(e.currentTarget.dataset));
 
     window.location = url;
-
-    console.log(e.currentTarget);
-}
-
+};
 
 const pega_json = async (caminho) => {
     const resposta = await fetch(caminho);
     const dados = await resposta.json();
     return dados;
-}
+};
 
 const montaCard = (atleta) => {
     const cartao = document.createElement("article");
     const nome = document.createElement("h1");
     const imagem = document.createElement("img");
     const descricao = document.createElement("p");
-    const link = document.createElement("a");
 
     nome.innerHTML = atleta.nome;
-    nome.style.fontFamily= 'sains-serif';
+    nome.style.fontFamily = 'sans-serif';
     cartao.appendChild(nome);
 
     imagem.src = atleta.imagem;
@@ -44,10 +46,6 @@ const montaCard = (atleta) => {
     descricao.innerHTML = atleta.detalhes;
     cartao.appendChild(descricao);
 
-    // link.innerText = 'Saiba Mais';
-    // link.href = `detalhes.html?id=${atleta.id}`;
-    // cartao.appendChild(link);
-
     cartao.onclick = manipulaClick;
 
     cartao.dataset.id = atleta.id;
@@ -55,27 +53,47 @@ const montaCard = (atleta) => {
     cartao.dataset.altura = atleta.altura;
 
     return cartao;
-}
+};
 
-pega_json(`${url}masculino`).then( 
-    (r) => {
-        r.forEach(
-            (ele) => container.appendChild(montaCard(ele))
-        )
+const verificaLogin = () => {
+    const logado = localStorage.getItem("logado");
+
+    if (logado === "sim") {
+        loginForm.style.display = "none";
+        jogadoresSection.style.display = "block";
+        botaoLogout.style.display = "block";
+        loginHeader.style.display = "block";
+        loginPage.style.display = "none";
+        login.style.display = "none";
+
+        pega_json(`${url}masculino`).then((r) => {
+            r.forEach((ele) => container.appendChild(montaCard(ele)));
+        });
+    } else {
+        loginForm.style.display = "block";
+        jogadoresSection.style.display = "none";
+        botaoLogout.style.display = "none";
+        loginHeader.style.display = "none";
     }
-);
+};
 
 const manipulaBotao = () => {
     const texto = document.getElementById("senha").value;
-    if (hex_md5(texto) === "5029cc9dd0295ded2f500084635c18c1"){
-        sessionStorage.setItem("logado", "sim")
+
+    if (hex_md5(texto) === "841775103f2d2eb0e244728e8efa7905") {
+        localStorage.setItem("logado", "sim");
+        verificaLogin();
     } else {
-        alert("Senha Incorreta!")
+        alert("Senha Incorreta!");
     }
-}
+};
 
 document.getElementById("botao").onclick = manipulaBotao;
 
 document.getElementById("logout").onclick = () => {
-    sessionStorage.removeItem("logado");
-}
+    localStorage.removeItem("logado");
+
+    window.location.reload();
+};
+
+document.addEventListener("DOMContentLoaded", verificaLogin);
